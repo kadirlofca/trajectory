ede742b38808429ff9dff5af371087d1137f5549
Kadir Lofca <kadirlofca@outlook.com>
2024-03-14 22:34:55 -0400

    Centered sticky notes

--- src/app/page.js
+++ src/app/page.js
@@ -8,16 +8,15 @@ export default async function Projects() {
   revalidatePath('/', 'layout')
 
   const pb = new PocketBase('http://127.0.0.1:8090')
-  const projects = (await pb.collection('projects').getFullList()).map((project) =>
+  const projects = (await pb.collection('projects').getFullList()).reverse().map((project) =>
     <StickyNote projectData={project} />
   );
 
   return (
     <div className="w-full h-full">
-      <div className='flex flex-wrap mx-auto max-w-screen-lg'>
+      <div className='flex flex-wrap justify-center mx-auto max-w-screen-lg'>
         {projects}
       </div>
-
     </div >
   );
 }
