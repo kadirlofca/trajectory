c92901cd131fe2b5c84befe3110e813b135c53c6
Kadir Lofca <kadirlofca@outlook.com>
2024-03-14 22:26:11 -0400

    made projects page the initial screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import Link from "next/link";
+import StickyNote from '@/app/components/StickyNote';
+import PocketBase from 'pocketbase'
+import { revalidatePath } from 'next/cache';
 
-export default async function Home() {
+export const dynamic = 'force-static'
+
+export default async function Projects() {
+  revalidatePath('/', 'layout')
+
+  const pb = new PocketBase('http://127.0.0.1:8090')
+  const projects = (await pb.collection('projects').getFullList()).map((project) =>
+    <StickyNote projectData={project} />
+  );
+
   return (
     <div className="w-full h-full">
-      <Link href={"pages/projects"}> goto projects </Link>
-    </div>
+      <div className='flex flex-wrap mx-auto max-w-screen-lg'>
+        {projects}
+      </div>
+
+    </div >
   );
 }
