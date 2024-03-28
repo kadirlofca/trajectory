912b6066bd5dd842b44ece7198a306b9fd2fc245
Hayden Perusek <111520459+Tangeloss@users.noreply.github.com>
2024-03-28 00:28:18 -0400

    update button

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -33,15 +33,19 @@ const buttonVariants = cva(
   }
 )
 
-const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : "button"
+const Button = React.forwardRef(({ className, variant, size, ...props }, ref) => {
   return (
-    (<Comp
+    <button
       className={cn(buttonVariants({ variant, size, className }))}
       ref={ref}
-      {...props} />)
+      {...props}
+    />
   );
-})
+});
+
+
+
+
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
