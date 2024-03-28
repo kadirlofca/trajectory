e9c9126a430aaf6abea7c9aac9973af0d879276d
kennedy <kennkenn0715@gmail.com>
2024-03-28 13:29:33 -0400

    footer

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,17 @@ export default async function Projects() {
 
   return (
     <div>
-      <div className="flex flex-wrap justify-center gap-12 max-w-screen-lg mx-auto mb-4 py-4 pl-8 border-b-2 border-gray-400 bg-gray-400">
+      <div className="w-full h-full">
+        <div className="flex flex-wrap justify-center mx-auto max-w-screen-lg">
+          <AddProjectForm />
+        </div>
+        <div className="flex flex-wrap justify-center pt-5 mb-4 mx-auto max-w-screen-lg">
+          {projects}
+        </div>
+      </div>
+
+      <footer className="bg-gray-400 border-t mt-40 border-gray-400">
+      <div className="flex flex-wrap justify-center gap-12 max-w-screen-lg mx-auto mb-4 py-4 pl-8">
         <div className="max-w-[29%]">
           Trajectory helps you organize and keep track of your car projects.
         </div>
@@ -26,15 +36,7 @@ export default async function Projects() {
           Kadir Lofca - Kennedy Ninh - Hayden Perusek
         </div>
       </div>
-
-      <div className="w-full h-full">
-        <div className="flex flex-wrap justify-center mx-auto max-w-screen-lg">
-          <AddProjectForm />
-        </div>
-        <div className="flex flex-wrap justify-center pt-5 mb-4 mx-auto max-w-screen-lg">
-          {projects}
-        </div>
-      </div>
-    </div>
+    </footer>
+  </div>
   );
 }
