4c94ea382e09f8acd49dec3c7fbe4acdb0267788
kennedy <kennkenn0715@gmail.com>
2024-03-28 01:17:08 -0400

    more popovers

--- src/app/components/items/Product.jsx
+++ src/app/components/items/Product.jsx
@@ -2,18 +2,30 @@
 
 import * as React from "react"
 import { ProductMenu } from "../menus/ProductMenu"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 
 export function Product({ productData }) {
     const [open, setOpen] = React.useState(false)
 
     return (
         <div className="flex w-full flex-col items-start justify-between rounded-md ml-10 border px-4 py-3 sm:flex-row sm:items-center">
-            <p className="text-sm font-medium leading-none">
-                <span className="mr-2 rounded-lg bg-primary px-2 py-1 text-xs text-primary-foreground">
-                    Product
-                </span>
-                <span className="text-muted-foreground">${productData.budget} - {productData.title} - {productData.text}</span>
-            </p>
+            <Popover>
+                <p className="text-sm font-medium leading-none">
+                    <span className="mr-2 rounded-lg bg-primary px-2 py-1 text-xs text-primary-foreground">
+                        Product
+                    </span>
+                    <PopoverTrigger>
+                        <span className="text-muted-foreground">{productData.title}</span>
+                    </PopoverTrigger>
+                </p>
+                <PopoverContent align="end" className="w-[200px]">
+                    <div className="p-4">
+                        <h2><strong>{productData.title}</strong></h2>
+                        <p>Budget: ${productData.budget}</p>
+                        <p>Description: {productData.text}</p>
+                    </div>
+                </PopoverContent>
+            </Popover>
             <ProductMenu productID={productData.id} />
         </div>
     )
--- src/app/components/menus/ProductMenu.jsx
+++ src/app/components/menus/ProductMenu.jsx
@@ -5,6 +5,16 @@ import deleteProduct from "@/app/actions/item/deleteProduct"
 import { MoreHorizontal, Tags, Trash } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Input } from "@/components/ui/input"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
 
 export function ProductMenu({ productID }) {
     const [label, setLabel] = React.useState("category")
@@ -26,9 +36,62 @@ export function ProductMenu({ productID }) {
             <DropdownMenuContent align="end" className="w-[200px]">
                 <DropdownMenuLabel>Actions</DropdownMenuLabel>
                 <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                        Edit
-                    </DropdownMenuItem>
+                    <Popover>
+                        <PopoverTrigger>
+                            <DropdownMenuItem>
+                                Edit
+                            </DropdownMenuItem>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <div>
+                                <h4 className="font-medium text-center leading-none">Edit Product</h4>
+                            </div>
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+                                    <FormField
+                                        control={form.control}
+                                        name="title"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Title</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="Edit Title" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="text"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Text</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="Edit Text" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="budget"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Make</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="Edit Budget" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <Button type="submit">Submit</Button>
+                                </form>
+                            </Form>
+                        </PopoverContent>
+                    </Popover>
                     {/* <DropdownMenuItem>
                         Select Product
                     </DropdownMenuItem> */}
