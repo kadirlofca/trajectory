d63d70c4964932dc3139cf9ed95b6d176e3f468f
Kadir Lofca <kadirlofca@outlook.com>
2024-04-03 22:13:31 -0400

    added product mark backend

---
+++ src/pocketbase/pb_migrations/1712192184_updated_project_item.js
@@ -0,0 +1,34 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("jlg880t5xihmn0v")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "p8noxxw5",
+    "name": "field",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "none",
+        "selected",
+        "bought"
+      ]
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("jlg880t5xihmn0v")
+
+  // remove
+  collection.schema.removeField("p8noxxw5")
+
+  return dao.saveCollection(collection)
+})
